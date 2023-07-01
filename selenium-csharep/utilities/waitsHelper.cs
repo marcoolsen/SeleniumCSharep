4acd6c2c557f3852d66fc67bafc9d1d69a23e13c
marcoolsen <46571669+marcoolsen@users.noreply.github.com>
2023-06-30 20:45:04 -0600

    Update waitsHelper.cs

--- selenium-csharep/utilities/waitsHelper.cs
+++ selenium-csharep/utilities/waitsHelper.cs
@@ -12,6 +12,24 @@ namespace selenium_csharep.utilities
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeout));
             return (WebElement)wait.Until(ExpectedConditions.ElementToBeClickable(locator));
         }
+
+        public WebElement waitForVisility(By locator, int timeout, IWebDriver driver)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeout));
+            return (WebElement)wait.Until(ExpectedConditions.ElementIsVisible(locator));
+        }
+
+        public WebElement waitForElementExist(By locator, int timeout, IWebDriver driver)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeout));
+            return (WebElement)wait.Until(ExpectedConditions.ElementExists(locator));
+        }
+
+        public Boolean waitInvisibilityElement(By locator, int timeout, IWebDriver driver)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeout));
+            return (Boolean)wait.Until(ExpectedConditions.InvisibilityOfElementLocated(locator));
+        }
     }
 }
 
