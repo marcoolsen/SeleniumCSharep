84a6ee5d510165549d4fc43ab7597d96b568b98a
marcoolsen <46571669+marcoolsen@users.noreply.github.com>
2023-06-30 20:45:51 -0600

    Update JsonReader.cs

--- selenium-csharep/utilities/JsonReader.cs
+++ selenium-csharep/utilities/JsonReader.cs
@@ -4,24 +4,20 @@ namespace selenium_csharep.utilities
 {
 	public class JsonReader
 	{
-		public JsonReader()
-		{
-		}
+	public JsonReader()
+	{
+	}
 
         public string extractData(String tokenName)
         {
-
             String myJsonString = File.ReadAllText("utilities/testData.json");
 
             var jsonObject = JToken.Parse(myJsonString);
             return jsonObject.SelectToken(tokenName).Value<string>();
-
-
         }
 
         public string[] extractDataArray(String tokenName)
         {
-
             String myJsonString = File.ReadAllText("utilities/testData.json");
 
             var jsonObject = JToken.Parse(myJsonString);
